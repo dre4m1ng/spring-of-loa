aa35c1626763d527ba3f1f9d5fdb7ea6c6ee4742
je0nh <winter0pear@gmail.com>
2024-06-26 22:02:14 +0900

    refactor: 분석 결과

--- streamlit/lostark_dmg_analysis.py
+++ streamlit/lostark_dmg_analysis.py
@@ -1,117 +1,120 @@
+import cv2
 import streamlit as st
 import os
-import pandas as pd
+import tempfile
+import math
+from multiprocessing import Pool
 import plotly.express as px
 import plotly.graph_objs as go
+from lostark import lostark_api, ExtractDmg
+from Model import ExtractDmg
 import json
-from lostark import lostark_api
+from PIL import Image
 
-# 영상 업로드
-def save_video_file(directory, file):    
-    save_path = os.path.join(directory, file.name)
-    with open(save_path, 'wb') as f:
-        f.write(file.getvalue())
-    return st.success("저장 완료!")
+def process_frame(frame_data):
+    frame_number, frame = frame_data
+    model = ExtractDmg.Model(frame)
+    cri = model.img_preprocessing(type='critical')
+    nor = model.img_preprocessing(type='normal')
+    return frame_number, cri, nor
 
-# 분석 코드
-
-# 데미지 계산
-def sum_dmg(df, type):
-    """
-    df: 데이터 프레임
-    type: critical, normal
-    """
-    dmg = []
-    n = len(df)
-    for i in range(n):
-        val = df.loc[i][type]
-        if isinstance(val, str) and '\n' in val:
-            sum_dmg = sum(float(num) for num in val.split('\n') if num.isdigit())
-            dmg.append(sum_dmg)
-        elif isinstance(val, str) and val.isdigit():
-            dmg.append(int(val))
-        else:
-            dmg.append(0)
-    return dmg
-
-def dmg_count(df, type):
-    cnt = list(df[type])
-    filter_cnt = 0
-    
-    for _ in cnt:
-        if _ > 0:
-            filter_cnt += 1
-    return filter_cnt
+def process_frames_chunk(frames_chunk, process_count):
+    with Pool(process_count) as pool:
+        results = pool.imap_unordered(process_frame, frames_chunk)
+        return list(results)
 
 # streamlit main
 def main():
+    # local test
+    tumb_img = Image.open('./image/lostark_thumb.png')
     # page thumbnail
-    st.image('./image/lostark_thumb.png')
+    # tumb_img = Image.open('./streamlit/image/lostark_thumb.png')
+    st.image(tumb_img)
     st.markdown("## Lost ARK 데미지 영상 분석기")
 
-    raids = ['발탄', '비아키스', '쿠크세이튼', '아브렐슈드', '일리아칸', '카멘', '카양겔', '상아탑', '에키드나', '베히모스']
+    raids = {'발탄': 'valtan', '비아키스': 'vykas', '쿠크세이튼': 'kakul', '아브렐슈드': 'brel',
+             '일리아칸': 'akkan', '카멘': 'thaemaine', '카양겔': 'kayangel', '상아탑': 'ivory',
+             '에키드나': 'echidna', '베히모스': 'behemoth'}
+    raids_ls = list(raids.keys())
+    
+    diff = {'노말': 'normal', '하드': 'hard', '헬': 'hell'}
+    diff_ls = list(diff.keys())
     
     with st.form(key='my_form'):
         api_key = st.text_input(label='API Key')
         char_name = st.text_input(label='닉네임')
-        raid = st.selectbox('레이드', raids)
-        diffi = st.selectbox('난이도', ['노말', '하드', '헬'])
+        raid = st.selectbox('레이드', raids_ls)
+        diffi = st.selectbox('난이도', diff_ls)
         gate = st.selectbox('관문', [1, 2, 3, 4])
 
-        save_path = './data'
-        video_data = st.file_uploader("", type=['jpg', 'csv', 'mp4'])
-
+        video_data = st.file_uploader("", type=['mp4'])
+        
         if video_data is not None:
-            save_video_file(save_path, video_data)
+            temp_dir = tempfile.mkdtemp()
+            video_path = os.path.join(temp_dir, video_data.name)
+            
+            with open(video_path, 'wb') as f:
+                f.write(video_data.getbuffer())
+            
+            video = cv2.VideoCapture(video_path)
+            total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            # progress_bar = st.progress(0)
+            
+            frames = []
+            result = []
+            frame_number = 0
+            frame_interval = 10
 
-            df = pd.read_csv(save_path + '/' + video_data.name)
+            while frame_number < total_frames:
+                ret, frame = video.read()
+                if not ret:
+                    break
 
+                if frame_number % frame_interval == 0:
+                    frame = cv2.resize(frame, dsize=(720, 480))
+                    frames.append((frame_number, frame))
+                
+                if len(frames) == 2000:
+                    process = process_frames_chunk(frames, 4)
+                    result += process
+                    frames = []
+                
+                frame_number += 1
+            
+            if frames:
+                process = process_frames_chunk(frames, 4)
+                result += process
+            
+            result.sort(key=lambda x: x[0])
+            
+            cri_cnt = 0
+            nor_cnt = 0
+
+            cri_ls = []
+            nor_ls = []
+            time_ls = []
+            for i in range(len(result)):
+                time = round(result[i][0] * 0.01666667, 2)
+                cri_dmg = result[i][1]
+                nor_dmg = result[i][2]
+                
+                time_ls.append(time)
+                nor_ls.append(nor_dmg)
+                cri_ls.append(cri_dmg)
+                
+                if cri_dmg > 0:
+                    cri_cnt += 1
+                if nor_dmg > 0:
+                    nor_cnt += 1
+            
         submit_button = st.form_submit_button(label='분석 시작!')
 
     if submit_button:
-        lostark = lostark_api.API(char_name, 'bearer ' + api_key)
+        lostark = lostark_api.API(char_name, api_key)
+        
+        user_class = lostark.profile.get('CharacterClassName')
 
-        user_profile = lostark.profile()
-
-        user_class = user_profile.get('CharacterClassName')
-        user_lv = user_profile.get('ItemAvgLevel')
-
-        user_equip = lostark.equipment()
-
-        el_sum = 0
-        chowal = 0
-        if float(user_lv.replace(',', '')) >= 1600:
-            for i in range(6):
-                eq_json = json.loads(user_equip[i].get('Tooltip'))
-
-                eq_dict = {}
-                for key, value in eq_json.items():
-                    eq_dict[key] = value
-
-                if i != 0:
-                    try:
-                        el01_lv = int(eq_dict.get('Element_010').get('value').get('Element_000').get('contentStr').get('Element_000').get('contentStr').split('</FONT>')[1][-1])
-                        el02_lv = int(eq_dict.get('Element_010').get('value').get('Element_000').get('contentStr').get('Element_001').get('contentStr').split('</FONT>')[1][-1])
-                    except (AttributeError, ValueError):
-                        try:
-                            el01_lv = int(eq_dict.get('Element_009').get('value').get('Element_000').get('contentStr').get('Element_000').get('contentStr').split('</FONT>')[1][-1])
-                            el02_lv = int(eq_dict.get('Element_009').get('value').get('Element_000').get('contentStr').get('Element_001').get('contentStr').split('</FONT>')[1][-1])
-                        except (AttributeError, ValueError):
-                            try:
-                                el01_lv = int(eq_dict.get('Element_008').get('value').get('Element_000').get('contentStr').get('Element_000').get('contentStr').split('</FONT>')[1][-1])
-                                el02_lv = int(eq_dict.get('Element_008').get('value').get('Element_000').get('contentStr').get('Element_001').get('contentStr').split('</FONT>')[1][-1])
-                            except AttributeError:
-                                pass
-                    el_sum += el01_lv + el02_lv
-
-                if float(user_lv.replace(',', '')) >= 1620 and i == 1:
-                    try:
-                        chowal = int(eq_dict.get('Element_009').get('value').get('Element_000').get('contentStr').get('Element_001').get('contentStr').split('</img>')[-1].split('개')[0])
-                    except:
-                        try:
-                            chowal = int(eq_dict.get('Element_008').get('value').get('Element_000').get('contentStr').get('Element_001').get('contentStr').split('</img>')[-1].split('개')[0])
-                        except:
-                            pass
+        user_lv, el_sum, chowal = lostark.usr_info()
 
         st.markdown("**[케릭터 정보]**")
         st.write('레벨:', user_lv)
@@ -119,22 +122,12 @@ def main():
         st.write('엘릭서:', el_sum)
         st.write('초월:', chowal)
         
-        time = [round(i * 0.08339, 2) for i in range(len(df))]
-        critical = sum_dmg(df, 'critical')
-        normal = sum_dmg(df, 'normal')
-        
-        data = {'critical' : critical, 'normal' : normal}
-        f_df = pd.DataFrame(data=data, index=time)
-
-        critical_filter = f_df[f_df['critical'] != 0][['critical']]
-        normal_filter = f_df[f_df['normal'] != 0][['normal']]
-        
         fig = go.Figure()
         # critical graph
-        fig.add_trace(go.Scatter(x=critical_filter.index, y=critical_filter['critical'], mode='lines', name='Critical', marker=dict(color='#F1CA36', size=8)))
+        fig.add_trace(go.Scatter(x=time_ls, y=cri_ls, mode='lines', name='Critical', marker=dict(color='#F1CA36', size=8)))
         
         # normal graph
-        fig.add_trace(go.Scatter(x=normal_filter.index, y=normal_filter['normal'], mode='lines', name='Normal', marker=dict(color='#595959', size=8)))
+        fig.add_trace(go.Scatter(x=time_ls, y=nor_ls, mode='lines', name='Normal', marker=dict(color='#595959', size=8)))
         
         fig.update_layout(
             xaxis=dict(title='time (s)'),
@@ -143,16 +136,22 @@ def main():
         
         st.plotly_chart(fig)
         
-        # critical 확률 계산
-        critical_cnt = dmg_count(f_df, 'critical')
-        normal_cnt = dmg_count(f_df, 'normal')
-        
-        crt_per = round(critical_cnt / (critical_cnt + normal_cnt), 3) * 100
-        max_dmgs = int(max(critical_filter['critical']))
-        sum_dmgs = int(sum(normal_filter['normal']) + sum(critical_filter['critical']))
-        dps = round(sum_dmgs / time[-1], 2)
+        sum_dmgs = sum(cri_ls) + sum(nor_ls)
+        if cri_cnt + nor_cnt > 0:
+            crt_per = round(cri_cnt / (cri_cnt + nor_cnt), 3) * 100
+        else:
+            crt_per = 0
+        if cri_ls:
+            max_dmgs = max(cri_ls)
+        else:
+            max_dmgs = 0
+        if time_ls:
+            dps = round(sum_dmgs / time_ls[-1], 2)
+        else:
+            dps = 0
         
         with st.form(key='final'):
+            st.write('데미지 총합:', sum_dmgs)
             st.write('크리티컬 확률 (%):', crt_per)
             st.write('최고 데미지:', max_dmgs)
             st.write('데미지 총합:', sum_dmgs)
